8aba193d2e840fd25d994751ca3509b5c4751eec
dyarfi <defrian.yarfi@gmail.com>
2017-08-01 18:53:57 +0700

    Home

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -27,80 +27,80 @@ class Home extends Public_Controller {
 
 	public function __construct() {
 		parent::__construct();
-		
+
 		// Load User related model in admin module
 		$this->load->model('admin/Users');
 		$this->load->model('admin/UserProfiles');
-		
+
 		// Load Setting data
 		$this->load->model('admin/Settings');
-		
+
 		// Load Career data
 		$this->load->model('career/Careers');
 
 		// Load User related model in admin module
 		$this->load->model('page/Pagemenus');
 		$this->load->model('page/Pages');
-		
+
 		/*
 		// Load IP2Location Class file without calling the _construct method
 		$this->load->file('application/libraries/IP2Location/ip2location.php');
-		
+
 		$loc = new IP2Location('application/libraries/IP2Location/databases/IP2LOCATION-LITE-DB11.BIN', IP2Location::FILE_IO);
-		
+
 		$ip = '139.0.26.20';
 
 		// Lookup for single field
 		echo 'Country Code: ' . $loc->lookup($ip, IP2Location::COUNTRY_CODE) . '<br />';
 		echo 'Country Name: ' . $loc->lookup($ip, IP2Location::COUNTRY_NAME) . '<br />';
-		
+
 		// Lookup for all fields
 		$record = $loc->lookup($ip, IP2Location::ALL);
 
 		echo '<pre>';
 		print_r($record);
 		echo '</pre>';
-		
+
 		exit;
-		 * 
+		 *
 		 */
-		
+
 		//print_r($this->session->userdata['session_id']);
 		//print_r('<br/>');
 		//print_r($this->session);
 
 		//$this->template->set_theme('default');
-		
-		
+
+
 	}
-	
+
 	public function index() {
-		
-					
+
+
 		// Set site title page with module menu
 		$data['page_title'] = $this->config->item('site_name') .' | '. $this->Settings->getByParameter('title_default')->value;
-		
+
 		// Set facebook link data
-		$data['vacancies']	= $this->Careers->getAllCareer();
+		//$data['vacancies']	= $this->Careers->getAllCareer();
 
 		// Set Gallery Data
-		$data['gallery'] 		= $this->Attachments->getAllAttachment('fabric');
-				
+		//$data['gallery'] 		= $this->Attachments->getAllAttachment('fabric');
+
 		// Set facebook link data
 		$data['facebook']	= $this->Settings->getByParameter('socmed_facebook');
-				
+
 		// Set twitter link data
 		$data['twitter']	= $this->Settings->getByParameter('socmed_twitter');
-		
+
 		// Set google link data
 		$data['google']		= $this->Settings->getByParameter('socmed_gplus');
-		
+
 		// Set contact email info data
-		$data['email_info']	= $this->Settings->getByParameter('email_info');		
-		
+		$data['email_info']	= $this->Settings->getByParameter('email_info');
+
 		// Set contactus address info data
-		$data['contactus_address']	= $this->Settings->getByParameter('contactus_address');		
-		
+		$data['contactus_address']	= $this->Settings->getByParameter('contactus_address');
+
 		// Set main template
 		$data['main'] = 'home';
 
@@ -113,40 +113,40 @@ class Home extends Public_Controller {
 			e.preventDefault();
 			$(this).find('.participated').hide();
 		})";
-		
+
 		// Load site template
-		$this->load->view('template/public/template', $this->load->vars($data));		
-		
+		$this->load->view('template/public/template', $this->load->vars($data));
+
 	}
-	
+
 	public function menu ($menu='') {
-		
+
 		// Set menu data
 		$data['menu'] = $menu;
-		
+
 		// Set pages data
 		$data['pages'] = $this->Pagemenus->getPagesByMenu($menu);
 		//exit('asdf');
 		// Set main template
 		$data['main'] = 'home';
-		
+
 		// Load admin template
 		$this->load->view('page', $this->load->vars($data));
 	}
-	
+
 	public function page ($menu='',$page='') {
-		
+
 		// Set menu data
 		$data['menu'] = $menu;
-		
+
 		// Set pages data
 		$data['page'] = $this->Pages->getPageByName($page);
-		
+
 		//print_r($data['pages']);
-		
+
 		// Set main template
 		//$data['main'] = 'page';
-		
+
 		// Load admin template
 		$this->load->view('page_detail', $this->load->vars($data));
 	}
--- application/views/home.php
+++ application/views/home.php
@@ -1,32 +1,12 @@
 <?php if (!defined('BASEPATH')) exit('No direct script access allowed'); ?>
-<?php
-/*
-* This is when user already participated
-*/
-if ($gallery) { ?>
 <div class="container">
   <div class="text-center main-block">
-    <h4>Gallery</h4>
-    <div class="row-fluid" style="margin:170px auto">
-      <ul class="list-unstyled list-inline">
-        <?php foreach ($gallery as $img) { ?>    
-        <li class="li-participated">
-          <a href="<?php echo base_url('uploads/gallery/'.$img->file_name);?>" id="fancybox" title="<?php echo $this->Participants->getParticipant($img->participant_id)->email;?>">
-            <img class="img-responsive" style="max-height:220px;<?php if ($attachment->id == $img->id) { echo 'border:2px dashed red;'; } ?>" src="<?php echo base_url('uploads/gallery/'.$img->file_name);?>"/>
-          </a>
-          <div class="participated">
-            <?php echo $this->Participants->getParticipant($img->participant_id)->email;?>
-          </div>          
-        </li>
-        <?php } ?>
-      </ul>
+    <img src="http://dentsudigital.co.id/images/logo/dxd3logo.png" alt="d3 Logo">
+    <div class="row-fluid" style="margin:100px auto 170px auto">
+        <h3>Campaign.Labs :</h3>
+        <a href="<?php echo base_url('fabric');?>"><h4>Fabric</h4></a>
+        <a href="<?php echo base_url('tracking');?>"><h4>Tracking</h4></a>
+        <a href="<?php echo base_url('quest');?>"><h4>Quest</h4></a>
     </div>
   </div>
 </div>
-<?php
-} else {
-/*
-* This is when user not yet participated
-*/
-}
-?>
